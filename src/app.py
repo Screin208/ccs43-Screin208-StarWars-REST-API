7b2662af1beb70c0a767948bc753adb1af411f32
Ruben Reyes <49405366+Screin208@users.noreply.github.com>
2023-10-04 12:43:47 +0000

    listo los metodos

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planets, Characters, FavPlanets, FavCharacters
 #from models import Person
 
 app = Flask(__name__)
@@ -45,6 +45,185 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+#Desde aqui creamos los Endpoints del Proyecto de StarWars
+#Metodo Get Listar todos los registros de people en la base de datos
+@app.route('/people', methods=['GET'])
+def all_character():
+    character = Characters.query.all()
+    arr_character = list(map(lambda people: people.serialize(), character))
+    print(arr_character)
+    return jsonify(arr_character), 200
+
+#Metodo Get Listar la información de una sola people
+@app.route('/people/<int:people_id>', methods=['GET'])
+def one_character(people_id):
+    one_people = Characters.query.get(people_id)
+    print(one_people)
+    return jsonify(one_people.serialize()), 200
+
+#Metodo Get Listar todos los registros de planetas en la base de datos
+@app.route('/planets', methods=['GET'])
+def all_planets():
+    planets = Planets.query.all()
+    arr_planets = list(map(lambda planets: planets.serialize(), planets))
+    print(arr_planets)
+    return jsonify(arr_planets), 200
+
+#Metodo Get Listar la información de un solo planeta
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def one_planet(planet_id):
+    one_world = Planets.query.get(planet_id)
+    print(one_world)
+    return jsonify(one_world.serialize()), 200
+
+#Metodo POST para crear los usuarios del blog
+@app.route('/create/user', methods=['POST'])
+def create_user():
+    body = request.json
+    name = body.get("name")
+    email = body.get("email")
+    password = body.get("password")
+
+    user = User(
+        name = name,
+        email = email,
+        password = password
+    )
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({
+            "message": "ERROR INTERNO",
+            "error": error.args
+        }), 500
+    return jsonify({}), 201
+
+#Metodo GET Listar todos los usuarios del blog
+@app.route('/users', methods=['GET'])
+def all_users():
+    user = User.query.all()
+    arr_user = list(map(lambda person: person.serialize(), user))
+    
+    return jsonify(arr_user), 200
+
+#Metodo GET Listar todos los favoritos que pertenecen al usuario actual
+@app.route('/users/favorites/<int:users_id>', methods=['GET'])
+def user_favotites(users_id):
+
+    # Localizar el usuario sus favoritos
+
+    # PLANET FAVORITO
+    planet_favorite = FavPlanets.query.filter_by(user_id = users_id)
+    planet = [planets.serialize() for planets in planet_favorite]
+
+    # CHARACTERS FAVORITO
+    character_favorite = FavCharacters.query.filter_by(user_id = users_id)
+    character = [character.serialize() for character in character_favorite]
+
+    return jsonify("Favorites", planet, character), 200
+
+
+
+#Metodo POST Añade un nuevo planet favorito al usuario actual con el planet id = planet_id
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_fav_planet(planet_id):
+    select_planet= Planets.query.get(planet_id)
+    body =  request.json
+    id_user = body.get("id_user")
+    actual_user = User.query.get(id_user)
+
+    favorite_planet = FavPlanets(
+        user = actual_user,
+        planets = select_planet
+    )
+
+    try:
+        db.session.add(favorite_planet)
+        db.session.commit()
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({
+            "message": "ERROR INTERNO",
+            "error": error.args
+        })
+    return jsonify({}), 200
+
+#Metodo POST Añade una nueva people favorita al usuario actual con el people.id = people_id
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def add_fav_character(characters_id):
+    select_character = Characters.query.get(characters_id)
+    body = request.json
+    id_user = body.get("id_user")
+    actual_user = User. query.get(id_user)
+
+    fav_character = FavCharacters(
+        user = actual_user,
+        characters = select_character
+    )
+    
+    try:
+        db.session.add(fav_character)
+        db.session.commit()
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({
+            "message": "ERROR INTERNO",
+            "error": error.args
+        })
+    return jsonify({}), 200
+
+
+#Metodo DELETE Elimina un planet favorito con el id = planet_id
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_fav_planet(planet_id):
+    delete_planet = FavPlanets.query.get(planet_id)
+
+    try:
+        db.session.delete(delete_planet)
+        db.session.commit()
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({
+            "message": "ERROR INTERNO",
+            "error": error.args
+        })
+    return jsonify({}), 200
+
+#Metodo DELETE  Elimina una people favorita con el id = people_id
+
+@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
+def delete_fav_character(characters_id):
+    delete_character = FavCharacters.query.get(characters_id)
+
+    try:
+        db.session.delete(delete_character)
+        db.session.commit()
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({
+            "message": "ERROR INTERNO",
+            "error": error.args
+        })
+    return jsonify({}), 200
+"""
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    todos.append(request_body)
+    json_body = jsonify(todos)
+    return json_body
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.pop(position - 1)
+    json_todos = jsonify(todos)
+    return json_todos
+"""
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -2,18 +2,135 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(db.Model):#(FATHER)
+    __tablename__ = "user"
+
     id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    #Relacion con FavPlanets 
+    favPlanets = db.relationship("FavPlanets", backref="user")
+    #Relacion con FavCharacters 
+    favCharacters = db.relationship("FavCharacters", backref="user")
 
+#Esto se debe repetir en cada tabla
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+#Aqui debemos crear nuestras tablas para las relaciones
+class Planets(db.Model): #(FATHER)
+    __tablename__ = "planets"
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    populations = db.Column(db.String(50), unique=False, nullable=False)
+    rotation_period = db.Column(db.String(50), unique=False, nullable=False)
+    orbital_period = db.Column(db.String(50), unique=False, nullable=False)
+    diameter = db.Column(db.String(50), unique=False, nullable=False)
+    gravity = db.Column(db.String(50), unique=False, nullable=False)
+    terrain = db.Column(db.String(50), unique=False, nullable=False)
+    surface_water = db.Column(db.String(50), unique=False, nullable=False)
+    climate = db.Column(db.String(50), unique=False, nullable=False)
+
+    #Relacion con el FavPlanets (Father)
+    favPlanets = db.relationship("FavPlanets", backref="planets")
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "created": self.created,
+            "populations": self.populations,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "diameter": self.diameter,
+            "gravity": self.gravity,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water,
+            "climate": self.climate,
+        }
+
+class FavPlanets(db.Model):
+    __tablename__ = "favPlanets"
+  
+    id = db.Column(db.Integer, primary_key=True)
+    #Relacion con la tabla User
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    #Relacion con la tabla de Planets
+    planets_id = db.Column(db.Integer, db.ForeignKey("planets.id"))
+
+
+    def __repr__(self):
+        return '<FavPlanets %r>' % self.id
+
+    def serialize(self):
+        return {
+            "planets": self.planets.serialize(),
+        }
+
+class Characters(db.Model): #(Father)
+    __tablename__ = "characters"
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(50), unique=True, nullable=False)
+    birth_year = db.Column(db.String(50), unique=False, nullable=False)
+    species = db.Column(db.String(50), unique=False, nullable=False)
+    height = db.Column(db.String(50), unique=False, nullable=False)
+    mass = db.Column(db.String(50), unique=False, nullable=False)
+    gender = db.Column(db.String(50), unique=False, nullable=False)
+    hair_color = db.Column(db.String(50), unique=False, nullable=False)
+    skin_color = db.Column(db.String(50), unique=False, nullable=False)
+    homeworld = db.Column(db.String(50), unique=False, nullable=False)
+    
+    #Relacion con FavCharacters 
+    favCharacters = db.relationship("FavCharacters", backref="characters")
+
+    def __repr__(self):
+        return '<Characters %r>' % self.full_name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "full_name": self.full_name,
+            "birth_year": self.birth_year,
+            "species": self.species,
+            "height": self.height,
+            "mass": self.mass,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "homeworld": self.homeworld,
+            "created": self.created,
+        }
+    
+class FavCharacters(db.Model):
+    __tablename__ = "favCharacters"
+
+    id = db.Column(db.Integer, primary_key=True)
+    #Relacion con la tabla User
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    characters_id = db.Column(db.Integer, db.ForeignKey("characters.id"))
+
+    def __repr__(self):
+        return '<FavCharacters %r>' % self.id
+
+    def serialize(self):
+        return {
+            "character": self.character.serialize()
+        }
+    
